1bb1fac4fbc18009a53edd10e6bfa99298508173
Flux73 <salahmoumni5@gmail.com>
2022-01-21 20:27:25 +0100

    New features

--- admin/js/script.js
+++ admin/js/script.js
@@ -5,3 +5,17 @@ ClassicEditor.create(document.querySelector("#body"))
   .catch((error) => {
     console.error(error);
   });
+
+$(document).ready(function () {
+  $("#select-all-boxes").click(function (e) {
+    if (this.checked) {
+      $(".checkBoxes").each(function () {
+        this.checked = true;
+      });
+    } else {
+      $(".checkBoxes").each(function () {
+        this.checked = false;
+      });
+    }
+  });
+});
